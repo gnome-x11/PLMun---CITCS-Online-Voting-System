398f2c907808dbdf8866374f4b32a52e5915dd57
Dexter Aromin <142196171+gnome-x11@users.noreply.github.com>
2024-08-28 13:26:02 +0800

    Add files via upload

--- db/votesystem.sql
+++ db/votesystem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 07:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.15
+-- Host: localhost
+-- Generation Time: Aug 28, 2024 at 04:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-
+-- Database: `votesystem`
 --
 
 -- --------------------------------------------------------
@@ -36,14 +35,14 @@ CREATE TABLE `admin` (
   `lastname` varchar(50) NOT NULL,
   `photo` varchar(150) NOT NULL,
   `created_on` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
-(1, 'nurhodelta', '$2y$10$fLK8s7ZDnM.1lE7XMP.J6OuPbQ.DPUVKBo7rENnQY7gYq0xAzsKJy', 'Neovic', 'Devierte', 'facebook-profile-image.jpeg', '2018-04-02');
+(1, 'admin@citcs', '$2y$10$j5V6UsjRaGwMZC9jyxX9VeLiZ6zM7DRxfzOYSHJODQRM0JXa9KsYi', 'Admin', 'CITCS', 'CITCS.jpeg', '2024-08-26');
 
 -- --------------------------------------------------------
 
@@ -58,7 +57,23 @@ CREATE TABLE `candidates` (
   `lastname` varchar(30) NOT NULL,
   `photo` varchar(150) NOT NULL,
   `platform` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`) VALUES
+(25, 10, 'Alice ', 'Guo', 'Alice_Guo.jpg', 'Lumaki sa farm. All natural walang halong chemical.'),
+(26, 10, 'Mang', 'Boy', 'Mang_Boy.jpg', 'Bawal Videoke sa New Year'),
+(27, 11, 'Diwata', 'Pares', 'Diwata_Pares.jpg', 'Birthday mo? Lilipas din Yan'),
+(28, 13, 'Mima', 'Otlum', 'Otlum.jpg', 'Ito ang simula'),
+(29, 14, 'Bato', 'Dela Rosa', 'Bato_Dela_Rosa.jpg', 'Wag mag bato'),
+(30, 14, 'Robin', 'Padilla', 'robin_hood.jpg', 'Yung Urge ba talaga, yung Urge'),
+(31, 14, 'Sprite', 'Coke', '', 'To make students happy'),
+(32, 10, 'Lucy ', 'Pher', '', 'Mag implement ng claygo sa rooms'),
+(33, 12, 'Wanwan', 'Guo', '', 'Magsulat nang magsulat'),
+(34, 12, 'Ele', 'Pante', '', 'ako na mag susualt, itabi nyo');
 
 -- --------------------------------------------------------
 
@@ -71,7 +86,18 @@ CREATE TABLE `positions` (
   `description` varchar(50) NOT NULL,
   `max_vote` int(11) NOT NULL,
   `priority` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `positions`
+--
+
+INSERT INTO `positions` (`id`, `description`, `max_vote`, `priority`) VALUES
+(10, 'President', 1, 1),
+(11, 'Vice President', 1, 2),
+(12, 'Secretary', 1, 3),
+(13, 'Treasurer', 1, 4),
+(14, 'P.R.O', 2, 5);
 
 -- --------------------------------------------------------
 
@@ -86,7 +112,16 @@ CREATE TABLE `voters` (
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
   `photo` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`id`, `voters_id`, `password`, `firstname`, `lastname`, `photo`) VALUES
+(8, 'jNt7eSwKghP3XGI', '$2y$10$Vy8qXomYsTom1fMgtV2kSO5wksBp45hbloKRncXQ5Wq0E326mGa8u', 'Elephant', 'Wanwan', ''),
+(9, 'xqsUCAv9wcfPG48', '$2y$10$Prq2dAZjYJ4EKIr3nTpHTuxTQ/fADoRu1oILl0zTLiYPtTCV0KCm6', 'Joe', 'Mama', ''),
+(10, '34Kj1ZokytQLOE7', '$2y$10$LOxOZ8R3y0fmv7cT1b.cpOi1UMg.iwdJT.dE3J6jOphN9TtvHTu06', 'Raffy', 'Tulfo', '');
 
 -- --------------------------------------------------------
 
@@ -99,7 +134,20 @@ CREATE TABLE `votes` (
   `voters_id` int(11) NOT NULL,
   `candidate_id` int(11) NOT NULL,
   `position_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `voters_id`, `candidate_id`, `position_id`) VALUES
+(81, 3, 19, 9),
+(82, 9, 32, 10),
+(83, 9, 27, 11),
+(84, 9, 34, 12),
+(85, 9, 28, 13),
+(86, 9, 29, 14),
+(87, 9, 31, 14);
 
 --
 -- Indexes for dumped tables
@@ -149,25 +197,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
